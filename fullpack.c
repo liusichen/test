e89737d73c976e32bc1a5da5684eba8f62454d0b
Sichen Liu <guijiao1234@gmail.com>
2017-03-31 11:00:47 +0800

    1043 fullpack price is inlimited

---
+++ fullpack.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int numofprice;
+    int lottery;
+    scanf("%d",&numofprice);
+    scanf("%d",&lottery);
+    int need[numofprice];
+    int value[numofprice];
+    int i,j;
+    int best[lottery+1];
+
+    for(i = 0; i < numofprice; i++){
+	scanf("%d",&need[i]);
+	scanf("%d",&value[i]);
+    }
+    
+    for(j = 0; j <= lottery; j++)best[j] = 0;
+    for(i = 0; i < numofprice; i++){
+	for(j = 1; j <= lottery; j++){
+	    if(need[i] < j){
+		best[j]= best[j]>0?best[j]:0;    
+	    }
+	    else{
+		best[j]=best[j]>(best[j-need[i]]+value[i])?best[j]:(best[j-need[i]]+value[i]);
+	    }
+	}
+    }
+    printf("%d\n",best[lottery]);
+    return 0;
+}
